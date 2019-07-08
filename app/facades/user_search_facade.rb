cc58e57cd86975a19f6624263fef0d619bb4d8ff
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-08 10:25:04 -0600

    Adds connection with Faraday

--- app/facades/user_search_facade.rb
+++ app/facades/user_search_facade.rb
@@ -1,8 +1,18 @@
 class UserSearchFacade
-  
 
-  def food_count
-    binding.pry
+def food_count
+  binding.pry
+end
+
+
+
+private
+  def conn
+    Faraday.new(:url => 'https://developer.nrel.gov/api') do |f|
+      req.headers['X-API-KEY']= ENV["DATA_GOV_API_KEY"]
+      f.adapter  Faraday.default_adapter
+    end
+    response = conn.get('https://developer.nrel.gov/api/alt-fuel-stations/v1.json?limit=1&api_key=ENV["DATA_GOV_API_KEY"]')
   end
 
 
