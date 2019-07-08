fdfb653e08c7e0831277ab6e271b1fb5e0e02925
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-08 10:40:04 -0600

    Restructuring user search facade to try and get to data

--- app/facades/user_search_facade.rb
+++ app/facades/user_search_facade.rb
@@ -1,18 +1,31 @@
 class UserSearchFacade
-
-def food_count
-  binding.pry
-end
-
-
-
-private
-  def conn
-    Faraday.new(:url => 'https://developer.nrel.gov/api') do |f|
-      req.headers['X-API-KEY']= ENV["DATA_GOV_API_KEY"]
-      f.adapter  Faraday.default_adapter
+  def initialize(food)
+    @food = food
+    @connection = Faraday.new(:url => 'https://developer.nrel.gov/api') do |f|
+        req.headers['X-API-KEY']= ENV["DATA_GOV_API_KEY"]
+        f.adapter  Faraday.default_adapter
     end
-    response = conn.get('https://developer.nrel.gov/api/alt-fuel-stations/v1.json?limit=1&api_key=ENV["DATA_GOV_API_KEY"]')
+  end
+
+  def get_url
+    @connection.get('https://www.data.gov/food/')
+  end
+
+  def retreive_data
+    get_url
+  end
+
+
+
+
+
+  private
+
+
+  def get_data
+    response = conn.get do |req|
+      binding.pry
+    end
   end
 
 
